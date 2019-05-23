ba9fb8d6d6e2dccd2953c842f385874ed576d369
CanosaLeandro <canosaleandro@gmail.com>
2019-05-23 06:04:06 -0300

    Correccion al pujar

--- Page/addPuja.php
+++ Page/addPuja.php
@@ -2,40 +2,34 @@
     Include("DB.php");
     $conexion = conectar();
 
-    $ID = $_POST['idS'];
+    $IDS = $_POST['idS'];
     $monto = $_POST['monto'];
 
-    $qryPuja = "SELECT puja_ganadora FROM subasta WHERE id =".$ID;
-    $result = mysqli_query($conexion, $qryPuja);
-    $pujaActual = mysqli_fetch_assoc($result)['puja_ganadora'];
-    
-    $qryActual = "SELECT monto FROM puja WHERE id = $pujaActual";
-    $resulta2 = mysqli_query($conexion, $qryActual);
-    $montoActual = mysqli_fetch_assoc($resulta2)['monto'];
+    $qry = "SELECT monto, puja_ganadora FROM subasta s INNER JOIN puja p ON s.puja_ganadora = p.id WHERE s.id =".$IDS;
+    $result = mysqli_query($conexion, $qry);
+    $registros = mysqli_fetch_assoc($result);
+    $pujaGanadora = $registros['puja_ganadora'];
+    $montoActual = $registros['monto'];
 
     if($montoActual < $monto) {
-        if(mysqli_query($conexion,"INSERT INTO puja VALUES ( , ,1, $ID , $monto )")){
-            $qryLastPuja = "SELECT id FROM puja WHERE monto = $monto";
-            $resultado = mysqli_query($conexion,$qryLastPuja);
-            $lastPuja = mysqli_fetch_assoc($resulatdo)['id'];
-            if (mysqli_query($conexion,"UPDATE subasta SET puja_ganadora= $lastPuja WHERE ID = $ID")){
-                ?>
-                <script> alert("La operación se completo correctamente");
-                    window.location = "subastas.php?id="+<?php echo($ID); ?>
-                </script>
-        <?php 
-            }
-        } else {
-        ?>  
-        <script> alert("No se pudo agregar el registo al sistema.");
-                window.location = "subasta.php?id="+<?php echo($ID); ?> ;
-        </script>
-<?php
-        }     
-    } else {
+        if(mysqli_query($conexion,"INSERT INTO puja SET id_usuario=1, id_subasta=$IDS, monto=$monto") &&
+            mysqli_query($conexion, "UPDATE subasta SET puja_ganadora= $pujaGanadora WHERE id = $IDS")){
 ?>
+                <script> alert("La operación se completo correctamente");
+                    window.location = "subastas.php?id="+<?php echo($IDS); ?>
+                </script>
+    <?php 
+            } else {
+    ?> 
+                <script> alert("ERROR!. La operacion no pudo realizarse"); 
+                    window.location = "subasta.php?id="+<?php echo($IDS); ?> ;
+                </script>
+    <?php
+            }
+    } else {
+    ?>
     <script> alert("ERROR!. El monto no supera la puja actual."); 
-            window.location = "subasta.php?id="+<?php echo($ID); ?> ;
+            window.location = "subasta.php?id="+<?php echo($IDS); ?> ;
     </script>
 <?php
     }
