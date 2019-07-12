c051466ccd2bb2dc8d50dffee5734a99cda89aa0
Tomiello <matias_lp_97@hotmail.com>
2019-07-12 16:46:19 -0300

    Se agrego la asignacion de los creditos con los usuarios al generar semanas

--- Page/habilitarNuevoUser.php
+++ Page/habilitarNuevoUser.php
@@ -18,19 +18,7 @@ try{
 $idUser=$_GET['id'];
 //habilito al nuevo usuario
 $query="UPDATE usuario SET valido='si' WHERE id=$idUser";
-<<<<<<< HEAD
-if(mysqli_query($conexion,$query)){
-	//selecciona los anio que hay en la tabla semana
-	$aniosVigentesQuery="SELECT DISTINCT anio FROM semana";
-	$aniosVigentes=mysqli_query($conexion,$aniosVigentesQuery);//para comparar año por año
 
-	//genero para cada año, los creditos para el nuevo usuario
-	while ($registroAnioVigente=mysqli_fetch_assoc($aniosVigentes)) {
-		$anio=$registroAnioVigente['anio'];
-		mysqli_query($conexion,"INSERT INTO creditos (id,id_usuario,anio,creditos) VALUES (NULL,$idUser,$anio,2)");
-	}
-	
-=======
 if(mysqli_query($conexion, $query)){
 	$aniosVigentesQuery="SELECT DISTINCT anio FROM semana";
 	$aniosVigentes=mysqli_query($conexion,$aniosVigentesQuery);//para comparar año por año
@@ -53,7 +41,7 @@ if(mysqli_query($conexion, $query)){
 		echo '<script>alert("ERROR al generar los creditos del año: '.$anio.'");
 			window.location = "habilitarUsuarios.php";</script>';
 	}
->>>>>>> f59cdc8557b6a849d0619433005c8e074c2f4ac7
+
 	echo "<script>alert('El nuevo usuario fue habilitado correctamente.');</script>";
 	echo "<script>window.location = 'habilitarUsuarios.php';</script>";	
 }else{
